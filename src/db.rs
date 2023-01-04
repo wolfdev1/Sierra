ab7fe74d3094876a82b1c08f4958174c17c58e96
wolfdev1 <wolfdev1@outlook.com>
2023-01-04 19:54:54 +0100

    MongoDB Secret removed

--- src/db.rs
+++ src/db.rs
@@ -3,7 +3,7 @@ use mongodb::*;
 
 pub async fn get_database() -> Result<Database, mongodb::error::Error> {
 
-    let mongodb_uri =  "mongodb://mongo:RDeXpyBeNFsHH6E8UScK@containers-us-west-168.railway.app:6976";
+    let mongodb_uri =  ""; // Your MongoDB URI here
     let client = Client::with_uri_str(mongodb_uri).await?;
     let db =  client.database("test");
 
